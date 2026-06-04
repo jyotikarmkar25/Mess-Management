5a0d6ff84e392fe7891d74cbf319864b2192387a
Mohan Kunjam <mohankunjam25@navgurukul.org>
2026-06-05 02:26:57 +0530

    hii

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
 app.use(express.json());
 
-const authRoutes = require("./routes/authRoutes");
+// Static Folder For Images
+app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
+// Routes
+const authRoutes = require("./routes/authRoutes");
+const attendanceRoutes = require("./routes/attendanceRoutes");
+const feedbackRoutes = require("./routes/feedbackRoutes");
+const menuRoutes = require("./routes/menuRoutes");
+const adminUserRoutes = require("./routes/adminUserRoutes");
+
+// Route Middleware
 app.use("/api/auth", authRoutes);
+app.use("/api/attendance", attendanceRoutes);
+app.use("/api/feedbacks", feedbackRoutes);
+app.use("/api/menus", menuRoutes);
+app.use("/api/admin/users", adminUserRoutes);
 
 module.exports = app;
